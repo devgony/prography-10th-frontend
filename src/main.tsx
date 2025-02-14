de2d10d0990568d78c459f0abd241b6c2e33cd2b
devgony <devgony@gmail.com>
2025-02-15 01:50:10 +0900

    chore: add react-router v7 configuration & base

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./Home";
+import Apply from "./Apply";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/apply" element={<Apply />} />
+    </Routes>
+  </BrowserRouter>,
+);
