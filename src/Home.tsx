1f5cc6e730216db89bd70d69a80aa098ca44c9be
devgony <devgony@gmail.com>
2025-02-15 11:32:35 +0900

    chore: configure automatic class sorting with prettier

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <img src="/prography.png" alt="prography-logo" />
-      <h1 className="text-xl text-blue-300 underline">Home</h1>
+      <h1 className="m-2 text-xl text-blue-300 underline">Home</h1>
     </>
   );
 }
