bdbc63a6e702dc828633de3d0f5228f0e82ebfc0
devgony <devgony@gmail.com>
2025-02-15 11:25:54 +0900

    feat: add prography logo & favicon

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
     <>
+      <img src="/prography.png" alt="prography-logo" />
       <h1 className="text-xl text-blue-300 underline">Home</h1>
     </>
   );
