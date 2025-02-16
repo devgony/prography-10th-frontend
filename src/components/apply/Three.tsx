e434dfadd1c9918806eb2281af5bd070f49214de
devgony <devgony@gmail.com>
2025-02-16 12:18:31 +0900

    refactor: extract enum Role

--- src/components/apply/Three.tsx
+++ src/components/apply/Three.tsx
@@ -1,6 +1,15 @@
 import Li from "../Li";
 import Title from "../Title";
 
+export enum Role {
+  Frontend = "프론트엔드",
+  Backend = "백엔드",
+  Design = "디자인",
+  iOS = "iOS",
+  Android = "안드로이드",
+  ProductOwner = "Product Owner",
+}
+
 export default function Three() {
   return (
     <section className="apply-main">
@@ -10,14 +19,7 @@ export default function Three() {
         <p>지원 분야를 선택해주세요</p>
 
         <ul className="flex flex-col gap-2">
-          {[
-            "프론트엔드",
-            "백엔드",
-            "디자인",
-            "iOS",
-            "안드로이드",
-            "Product Owner",
-          ].map((label) => (
+          {Object.values(Role).map((label) => (
             <Li key={label} label={label} />
           ))}
         </ul>
