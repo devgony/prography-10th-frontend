ca40b38e19c3cbc4eaceeecb63764b67b333f6d6
devgony <devgony@gmail.com>
2025-02-20 16:08:27 +0900

    feat: set max width to 3xl, mx-auto

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 
 export default function RootLayout() {
   return (
-    <div className="min-h-screen bg-gray-200 p-4">
+    <div className="mx-auto min-h-screen max-w-3xl bg-gray-200 p-4">
       <Outlet />
     </div>
   );
